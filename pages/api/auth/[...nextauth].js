6d167e2b1f9c232cb89752375c62ead544a0cac6
MaciejZebala <kropka551@op.pl>
2021-05-29 16:28:37 +0200

    feat: Add github authentication

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
+  ],
+  session: { jwt: true },
+  jwt: { encryption: true },
+  callbacks: {
+    jwt: async (token, user, account, profile) => {
+      if (user && account && account.provider === 'github') {
+        token.username = profile.login;
+        (token.githubAccessToken = account.accessToken),
+          (token.randomStuff = 'anything you want');
+      }
+
+      return Promise.resolve(token);
+    },
+    session: async (session, user) => {
+      return Promise.resolve({ ...session, user });
+    }
+  }
+});
