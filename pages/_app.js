39a47701eaad11762a90e0fc094e2d99e19c0687
MaciejZebala <kropka551@op.pl>
2021-05-30 11:58:00 +0200

    feat: Create apolloClient

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import '../styles/index.scss';
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from '../lib/apollo/apolloClient';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={createApolloClient()}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
